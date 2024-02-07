921678a3dc5afa13c0f4e786516338ed46db1cfd
RumenaStaneva <rumenastaneva2106@gmail.com>
2024-02-07 16:51:01 +0200

    used descriminated unions

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./CourseGoal";
 import { type CourseGoal as CGoal } from "../App";
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 
 type CouseGoalListProps = {
   goals: CGoal[];
@@ -10,15 +12,35 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CouseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You're collecting a lot of goals, don't put too much on your plate!
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal title={goal.title} onDelete={onDeleteGoal} id={goal.id}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title} onDelete={onDeleteGoal} id={goal.id}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
